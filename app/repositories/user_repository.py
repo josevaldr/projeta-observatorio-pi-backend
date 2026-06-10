4a31ff3d9947387b4a4111a6c9b50acce49fae0b
José Valdir <josevaldir.candido@gmail.com>
2026-06-10 11:06:11 -0300

    CRUD completo do user

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,5 +1,5 @@
-
 from app.config.database import get_connection
+
 
 def create_user(data):
     conn = get_connection()
@@ -32,3 +32,80 @@ def create_user(data):
     finally:
         cursor.close()
         conn.close()
+
+
+def get_user_by_id(user_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM usuario WHERE id_usuario = ?", (user_id,))
+        user = cursor.fetchone()
+
+        if user:
+            return dict(user)
+        return None
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def update_user(user_id, data):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        query = """
+        UPDATE usuario
+        SET nome_usuario = ?, email = ?, tipo_usuario = ?
+        WHERE id_usuario = ?
+        """
+
+        cursor.execute(query, (
+            data.nome_usuario,
+            data.email,
+            data.tipo_usuario,
+            user_id
+        ))
+
+        conn.commit()
+
+        return {"message": "Usuário atualizado com sucesso"}
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def delete_user(user_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("DELETE FROM usuario WHERE id_usuario = ?", (user_id,))
+        conn.commit()
+
+        return {"message": "Usuário deletado com sucesso"}
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def get_all_users():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM usuario")
+        users = cursor.fetchall()
+        return [dict(user) for user in users]
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+        
+
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,9 +1,31 @@
 from fastapi import APIRouter
-from app.controllers.user_controller import create_user_controller
 from app.schemas.user_schemas import UserCreate
+from app.controllers.user_controller import (
+    create_user_controller,
+    get_users_controller,
+    get_user_by_id_controller,
+    update_user_controller,
+    delete_user_controller
+)
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/")
 def create_user(data: UserCreate):
     return create_user_controller(data)
+
+@router.get("/{id}")
+def get_user(id: int):
+    return get_user_by_id_controller(id)
+
+
+@router.put("/{id}")
+def update_user(id: int, data: UserCreate):
+    return update_user_controller(id, data)
+
+
+@router.delete("/{id}")
+def delete_user(id: int):
+    return delete_user_controller(id)
+
