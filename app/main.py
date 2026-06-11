1f04cfdbc6a2a5b17252a029c2c687e7b1176dfa
MaveBunny <mavanacamilla@hotmail.com>
2026-06-10 22:23:02 -0300

    Corrige conflitos e duplicações da rota de empresa

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 
 from app.config.settings import settings
@@ -6,6 +5,8 @@ from app.config.init_db import create_tables
 
 from app.routes.user_routes import router as user_router
 from app.routes.aluno_routes import router as aluno_router
+from app.routes.professor_routes import router as professor_router
+from app.routes.coordenador_routes import router as coordenador_router
 from app.routes.empresa_routes import router as empresa_router
 
 
@@ -20,6 +21,8 @@ app = FastAPI(
 
 app.include_router(user_router)
 app.include_router(aluno_router)
+app.include_router(professor_router)
+app.include_router(coordenador_router)
 app.include_router(empresa_router)
 
 
